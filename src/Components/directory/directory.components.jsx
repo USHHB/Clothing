9da5a65312f8ebad0289e22c2c6baf6a629d75d9
opeyemiadebisi87 <opeyemiadebisi87@gmail.com>
2022-01-10 02:49:31 +0100

    created project with create react app and added folders for components and pages

---
+++ src/Components/directory/directory.components.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import MenuItem from "../menu-item/menu-item.component";
+import "./directory.styles.scss";
+import img from "../../../src/img.jpg";
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          title: "hats",
+          // imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          imageUrl: img,
+          id: 1,
+          linkUrl: "shop/hats",
+        },
+        {
+          title: "jackets",
+          // imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          imageUrl: img,
+          id: 2,
+          linkUrl: "shop/jackets",
+        },
+        {
+          title: "sneakers",
+          // imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          imageUrl: img,
+          id: 3,
+          linkUrl: "shop/sneakers",
+        },
+        {
+          title: "womens",
+          // imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          imageUrl: img,
+          size: "large",
+          id: 4,
+          linkUrl: "shop/womens",
+        },
+        {
+          title: "mens",
+          // imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          imageUrl: img,
+          size: "large",
+          id: 5,
+          linkUrl: "shop/mens",
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ title, id, imageUrl, linkUrl, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
